05347fa754f0e08ed0765cae18af02fea54b4ea3
Mark Coleman <m@rkcoleman.co.uk>
2019-10-15 16:43:37 +0100

    Add more flashing

--- scare_em.rb
+++ scare_em.rb
@@ -9,9 +9,31 @@ relay = YaGPIO.new(RELAY_SIGNAL, YaGPIO::OUTPUT)
 pir.set_interrupt(YaGPIO::EDGE_RISING) do |high|
     fork { exec("cvlc --play-and-exit #{ALARM_SOUND}") }
 
+    sleep 0.9
     relay.high
     sleep 0.3
     relay.low
+    sleep 0.3
+    relay.high
+    sleep 0.5
+    relay.low
+    sleep 0.3
+    relay.high
+    sleep 0.3
+    relay.low
+    sleep 0.4
+    relay.high
+    sleep 0.5
+    relay.low
+    sleep 0.4
+    relay.high
+    sleep 0.3
+    relay.low
+    sleep 0.3
+    relay.high
+    sleep 1.3
+    relay.low
+    sleep 20
 end
 
 puts 'ready'
